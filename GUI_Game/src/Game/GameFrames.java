4fd7d344b1c06937623b0ad6495909d995089135
txrth <4tirth.patel@gmail.com>
2020-12-27 17:35:11 -0500

    levels

--- GUI_Game/src/Game/GameFrames.java
+++ GUI_Game/src/Game/GameFrames.java
@@ -3,6 +3,7 @@ package Game;
 import java.awt.EventQueue;
 
 import javax.swing.JFrame;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
 
@@ -31,22 +32,21 @@ public class GameFrames extends JFrame {
 		setBounds(100, 100, 945, 668);
 		setResizable(false);
 
-		// int opt = 0;
-		// while (!(opt > 0 && opt <= 3)) {
-		// try {
-		// opt = Integer.parseInt(JOptionPane.showInputDialog(contentPane, "Select Level
-		// 1,2, or 3"));
-		// } catch (Exception e) {
+		int opt = 0;
+		while (!(opt > 0 && opt <= 3)) {
+			try {
+				opt = Integer.parseInt(JOptionPane.showInputDialog(contentPane, "Select Level 1,2, or 3"));
+			} catch (Exception e) {
 
-		// }
-		// }
-		// if (opt == 1) {
-		contentPane = new layout(8000, 500);
-		// } else if (opt == 2) {
-		// contentPane = new layout(6500, 200);
-		// } else {
-		// contentPane = new layout(5000, 100);
-		// }
+			}
+		}
+		if (opt == 1) {
+			contentPane = new layout(8000, 500);
+		} else if (opt == 2) {
+			contentPane = new layout(6500, 200);
+		} else {
+			contentPane = new layout(5000, 100);
+		}
 
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
